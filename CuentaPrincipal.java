99e20c6d28d3b80145f35b017a0df27daa24286e
FerYAlex <159776151+FerYAlex@users.noreply.github.com>
2024-03-21 18:05:35 +0100

    cambio de texto, 45 en lugar 40

--- CuentaPrincipal.java
+++ CuentaPrincipal.java
@@ -43,7 +43,7 @@ public class CuentaPrincipal {
 	
 				System.out.println("Su saldo actual es de "+ cuenta1.getSaldo());
 				System.out.println("¿Cuanto dinero quieres retirar?");
-				System.out.println("Introduzca el numero 1 para retirar 40€");
+				System.out.println("Introduzca el numero 1 para retirar 45€");
 				System.out.println("Introduzca el numero 2 para retirar 60€");
 				System.out.println("Introduzca el numero 3 para retirar 100€");
 				System.out.println("Introduzca el numero 4 para retirar 200€");
